882277b74af342b7cfaaf2ecfac9b8d039942daa
Karen Christina <karen.nascimento.developer@gmail.com>
2023-03-14 15:04:17 -0300

    fix: module and css improvements

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Course } from '../models/course';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  public courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'Front-end' },
+  ];
+  public displayedColumns: String[] = ['name', 'category'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
